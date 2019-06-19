17ed6370ba7656fda186139fe8b0a59943f01d4c
avalerab <avalerab@yahoo.com>
2019-06-19 11:22:10 -0400

    Mi primera subida Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,56 @@
 window.cipher = {
   encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+    function btncifrar() {
+      console.log(m);
+      let nss = parseInt(e);
+      //Chequeamos el texto, para convertir todas las letras en mayusculas
+      let mMayuscula =m.toUpperCase(); 
+      console.log(mMayuscula);
+      //Obtenemos el codigo ASCii de cada letra del mensaje  
+      let cifrado = "";
+      let resultadoASCii='';
+      let nuevoASCii='';
+  
+      for (let i=0; i<mMayuscula.length; i++){ 
+      resultadoASCii = mMayuscula.charCodeAt(i);
+      // Mostramos los ACSii en la consola
+      console.log("Esto es el ASCii original" + resultadoASCii);
+      nuevoASCii = ((resultadoASCii -65 +nss)%26 + 65);
+      console.log(nuevoASCii);
+      //Ciframos el mensaje
+      cifrado += String.fromCharCode(nuevoASCii);  
+      console.log(cifrado);
+     }
+     }  
+    },
+  
+ decode: ()=> {
+      //Decode: Descifrado de mensaje
+      function btndescifrar() {
+      console.log(m);
+      //Convertimos el Offset en numero
+      let nss = parseInt(e);
+      //Transformamos las letras del mensaje en mayusculas
+      let mMayuscula =m.toUpperCase(); 
+      console.log(mMayuscula);
+      //Obtenemos los codigos ASCii del mensaje a descifrar
+      let mdescifrado = "";
+      let resultadoASCii2='';
+      let nuevoASCii2='';
+
+      for (let i=0; i<mMayuscula.length; i++){ 
+      resultadoASCii2 = m.charCodeAt(i);
+      nuevoASCii2 = (resultadoASCii2 -nss);
+      //Mostramos los codigos ASCii en la consola
+      console.log(nuevoASCii2);
+      //Obtenemos el mensaje original o descifrado
+      mdescifrado += String.fromCharCode(nuevoASCii2);
+      //Mostramos el mensaje original en la consola
+      console.log("Tu mensaje original era: " +mdescifrado);
+      
+      }
+      }      
+
+    }
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-/*Acá va tu código*/
+ const btncifrar = document.getElementById("mcifrar");
+ btncifrar.addEventListener("click", ()=> {
+ //Traemos el mensaje a cifrar id=mCifrar desde el HTML
+ let m = document.getElementById("mCifrar").value;
+ //Traemos el Offset a emplear en la formula y lo convertimos en "numero" con el parseInt
+ let e = document.getElementById("ns").value;
+ //Mostramos el resultado en el HTML con el innerHTML 
+    document.getElementById("resultado").innerHTML = "Tu mensaje cifrado es: " +cifrado;
+ }
+ 
+ const btndescifrar = document.getElementById("mcifrar");
+ btndescifrar.addEventListener("click", () =>{
+   //Traemos el mensaje a descifrar (id=mCifrar) desde el HTML
+   let m = document.getElementById("mCifrar").value;
+   //Traemos el Offset desde el HTML
+    let e = document.getElementById("ns").value;
+   //Mostramos el mensaje original en el HTML (como el resultado esperado)
+   document.getElementById("resultado").innerHTML = "Tu mensaje descifrado es: " +mdescifrado;
+ }
